f0b0f64cbd1696f8e0fa28caa87a406d6f3a5782
BadGenius22 <badgenius.crypto@gmail.com>
2026-01-09 18:50:51 +0700

    chore(be): change to solscan

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,22 +7,22 @@ export const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL!;
 export const PORTAL_URL = process.env.NEXT_PUBLIC_PORTAL_URL!;
 export const PAYMASTER_URL = process.env.NEXT_PUBLIC_PAYMASTER_URL!;
 
-// Solana Explorer
-export const EXPLORER_BASE_URL = "https://explorer.solana.com";
+// Solscan Explorer
+export const EXPLORER_BASE_URL = "https://solscan.io";
 export const CLUSTER = "devnet";
 
 /**
- * Get Solana Explorer URL for a transaction signature
+ * Get Solscan URL for a transaction signature
  */
 export function getExplorerUrl(signature: string): string {
   return `${EXPLORER_BASE_URL}/tx/${signature}?cluster=${CLUSTER}`;
 }
 
 /**
- * Get Solana Explorer URL for an account address
+ * Get Solscan URL for an account address
  */
 export function getAccountUrl(address: string): string {
-  return `${EXPLORER_BASE_URL}/address/${address}?cluster=${CLUSTER}`;
+  return `${EXPLORER_BASE_URL}/account/${address}?cluster=${CLUSTER}`;
 }
 
 /**
